61dc357910aba9058569b045df00746cc0ca5347
AnomicAphasia <travis.briscoe@gmail.com>
2021-06-08 17:48:40 -0400

    editted index.html, package.json

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const publicStaticPath = path.join(__dirname, '../public');
 
-app.use(express.json());
+// app.use(express.json());
 app.use(express.static(publicStaticPath));
 
 app.listen(port, () => {
-  console.log(`Server started. Bind to port ${port}`);
+  console.log(`Server bind to port ${port}`);
 });
